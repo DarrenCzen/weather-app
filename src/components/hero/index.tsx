a1f08cb12e22b6266708e506eaee17b88df5f9c0
Darren <zen_darren@hotmail.com>
2023-10-13 23:47:16 +0800

    Commit label and input

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,41 +1,55 @@
-import { Rocket, Globe2, Wrench, Zap } from 'lucide-react'
+import { Search } from 'lucide-react'
 import { Button } from '../ui/button'
+import { Input } from '../ui/input'
+import { Label } from '../ui/label'
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Payment = {
+  id: string
+  amount: number
+  status: 'pending' | 'processing' | 'success' | 'failed'
+  email: string
+}
 
 export const Hero = () => {
   return (
     <div className="flex min-h-screen bg-gradient-to-b from-purple-200 to-purple-600">
-      <section className="w-full py-32 md:py-48">
+      <section className="w-full py-32 md:py-24">
         <div className="container px-4 md:px-6">
           <div className="grid items-center gap-6">
             <div className="flex flex-col justify-center space-y-4 text-center">
-              <div className="mb-24">
-                <h1 className="mb-6 text-3xl font-bold tracking-tighter text-transparent text-white sm:text-5xl xl:text-6xl/none">
-                  A Boilerplate Title
+              <div className="mb-12">
+                <h1 className="mb-6 text-3xl font-bold tracking-tighter text-transparent text-white sm:text-4xl xl:text-5xl/none">
+                  Weather Application
                 </h1>
-                <Button className="font-semiboldn gap-3 py-6 text-lg" size={'lg'} asChild>
-                  <a href="https://github.com/Quilljou/vite-react-ts-tailwind-starter">
-                    <Zap />
-                    Get Started
-                  </a>
-                </Button>
               </div>
-              <div className="mx-auto w-full max-w-full space-y-4">
-                <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
-                  <div className="flex flex-col items-center space-y-2 rounded-lg p-4">
-                    <div className="rounded-full bg-black p-4 text-white">
-                      <Rocket size={32} />
-                    </div>
+              <div className="mx-auto flex w-full max-w-3xl">
+                <div className="w-10/12 sm:w-11/12 sm:pr-5">
+                  <div className="text-left">
+                    <Label htmlFor="Country">Country</Label>
                   </div>
-                  <div className="flex flex-col items-center space-y-2 rounded-lg p-4">
-                    <div className="rounded-full bg-black p-4 text-white">
-                      <Globe2 size={32} />
+                  <Input type="Country" id="Country" placeholder="Country" />
+                </div>
+                <div className="w-2/12 text-end sm:w-1/12">
+                  <Button
+                    onClick={() => {
+                      console.log('here')
+                    }}
+                    className="rounded-2xl border-2 border-purple-700 bg-purple-700 py-7 hover:bg-purple-400"
+                    asChild
+                  >
+                    <div className="hover:text-black">
+                      <Search />
                     </div>
-                  </div>
-                  <div className="flex flex-col items-center space-y-2 rounded-lg p-4">
-                    <div className="rounded-full bg-black p-4 text-white">
-                      <Wrench size={32} />
-                    </div>
-                  </div>
+                  </Button>
+                </div>
+              </div>
+              <div className="container mx-auto max-w-3xl rounded-md border py-10">
+                <div className="container mx-auto rounded-md border py-10">
+                  <div className="text-left">Search History</div>
+                  <div className="container mx-auto my-10 rounded-md border py-10"></div>
+                  <div className="container mx-auto my-10 rounded-md border py-10"></div>
                 </div>
               </div>
             </div>
